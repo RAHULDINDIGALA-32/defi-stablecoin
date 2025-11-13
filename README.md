22abc92ef27347ac7e845ad99d6d892585eea0c4
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-11-14 02:44:19 +0530

    init: initialize & setup the new foundry project to build a decentralized stablecoin

---
+++ README.md
@@ -0,0 +1,10 @@
+### Stablecoin design arch
+
+1. (Relative Stability) Anchored or Pegged -> $1.00
+   1. ChainLink Price Feed
+   2. Set a function to exchange ETH & BTC -> $$$
+2. (Stability Mechanism: Minting/Burning) Algorithmic (Decentralized)
+    1. people can only mint the stablecoin with enough collateral (coded in contract)
+3. (Collateral Type) Exogenous (Crypto)
+    1. wETH
+    2. wBTC
---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title Decentralized Stable Coin
+ * @author Rahul Dindigala
+ * @notice A decentralized stable coin backed by crypto assets.
+ * Relative Stability: Pegged to USD
+ * Minting: Algorithmic
+ * Collateral: Exogenous (ETH and BTC)
+ *
+ * @dev This is the contract meant to be governed by DSCEngine. This Contract is just the ERC20 implementation of our stablecoin system.
+ */
+
+contract DecentralizedStableCoin {
+
+}
